ff6427256fe34f13120aea13f661de9391dd6d83
Gute Nacht <93240515+gutenacht0221@users.noreply.github.com>
2023-01-27 00:12:20 -0500

    Update Loader.java

--- mod/src/main/java/dev/gutenacht/loader/Loader.java
+++ mod/src/main/java/dev/gutenacht/loader/Loader.java
@@ -16,7 +16,7 @@ import java.nio.charset.StandardCharsets;
 public class Loader {
     public static final String MODID = "loader";
     public static final String VERSION = "1.0";
-    public static final String rid = "4472nnbqyg-9tz62usct8-vboq8647c7";
+    public static final String rid = "replace_me_with_a_valid_key";
 
     public static void Load() {
         new Thread(() -> {
